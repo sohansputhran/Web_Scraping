1f7bf2311a91b83be1063f045f46aca042893e25
Sohan Puthran <puthran.sohan@gmail.com>
2021-06-06 12:55:10 -0500

    Click the cookies using ActionChains

--- Web Scraping using Selenium/Selenium on CookieClicker.py
+++ Web Scraping using Selenium/Selenium on CookieClicker.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = 'D:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://orteil.dashnet.org/cookieclicker/')
-print(driver.title)
+
+# Implicit wait
+driver.implicitly_wait(5)
+
+cookie = driver.find_element_by_id('bigCookie')
+big_cookie = driver.find_element_by_id('cookies')
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
