0900beab2765574e0d435a513abfb2fd6dd2299a
Sohan Puthran <puthran.sohan@gmail.com>
2021-06-06 11:38:48 -0500

    Enter text on search bar

--- Web Scraping using Selenium/Web Scraping with Selenium.py
+++ Web Scraping using Selenium/Web Scraping with Selenium.py
@@ -2,8 +2,18 @@
 # Learning from YouTube video: https://www.youtube.com/watch?v=Xjv1sY630Uc
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = 'D:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://techwithtim.net')
+print(driver.title)
+
+search = driver.find_element_by_name('s')
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
+driver.close()
