d4d6fac8110115b996b4c33febc3605d2556c221
Sohan Puthran <puthran.sohan@gmail.com>
2021-06-06 13:00:51 -0500

    Cookie Clicker complete

--- Web Scraping using Selenium/Selenium on CookieClicker.py
+++ Web Scraping using Selenium/Selenium on CookieClicker.py
@@ -10,7 +10,7 @@ driver.get('https://orteil.dashnet.org/cookieclicker/')
 driver.implicitly_wait(5)
 
 cookie = driver.find_element_by_id('bigCookie')
-big_cookie = driver.find_element_by_id('cookies')
+cookie_count = driver.find_element_by_id('cookies')
 items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1)]
 
 actions = ActionChains(driver)
@@ -18,3 +18,12 @@ actions.click(cookie)
 
 for i in range(5000):
     actions.perform()
+    count = int(cookie_count.text.split(" ")[0])
+    print(count)
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
